539d6fa86028b0f867f0e217c2ec436bb2f55a2d
Francesco <alemannof43@gmail.com>
2025-01-13 17:41:36 +0100

    fixati comportamenti

--- football-app/src/componenti/Champions.jsx
+++ football-app/src/componenti/Champions.jsx
@@ -1,63 +1,89 @@
+import { useNavigate } from 'react-router-dom';
 import useSwr from 'swr';
 
-const fetcher= url=> fetch(url).then(response=>response.json())
+const fetcher = (url) => fetch(url).then((response) => response.json());
 
-export function Champions(){
+export function Champions() {
+  const { data, error, isLoading } = useSwr('https://api.openligadb.de/getmatchdata/ucl2024/2024/1', fetcher);
+const navigate= useNavigate()
+const navigateToHome= ()=>{
+    navigate('/home')}
+  if (error) {
+    return <p>Errore nel fetching dati</p>;
+  }
+  if (isLoading) {
+    return <p>Caricamento in corso</p>;
+  }
 
-const {data, error, isLoading}=useSwr('https://api.openligadb.de/getmatchdata/ucl2024/2024/1', fetcher)
-    
-if(error){
-    return <p>Errore nel fetching dati</p>
-}
-if(isLoading){
-    return <p>Caricamento in corso</p>
-}
-    return(
-        <div>
-<h3>Partite UCL 2024 </h3>
-{data.map((match)=>(
+  return (
+    <div className='champions-home'>
+        <button className='btn' onClick={navigateToHome}>Torna indietro</button>
+      <h3>Partite UCL 2024</h3>
 
-    <div>
-         
-        <div className='match-description'>
-            <p key={match.matchID}>{match.leagueName}</p>
-            <p key={match.matchID}>{match.group.groupName}</p>
-            <p key={match.matchID}>{new Date(match.matchDateTime).toLocaleString()}</p>
+      {data.map((match) => {
+      
+        const risultato = match.matchResults.find(
+          (result) => result.resultName === 'Endergebnis'
+        );
+
+        
+        if (!risultato) {
+          return <p key={match.matchID}>Risultato finale non disponibile per questa partita.</p>;
+        }
+
+        return (
+            
+                 <div key={match.matchID} >
+            <div className="match-description">
+              <p>{match.leagueName}</p>
+              <p>{match.group.groupName}</p>
+              <p>{new Date(match.matchDateTime).toLocaleString()}</p>
             </div>
 
+            <div className="champions">
+              <div className="team1">
+                <div className="team1-container">
+                  <img
+                    src={match.team1.teamIconUrl}
+                    width={35}
+                    alt={match.team1.teamName}
+                  />
+                </div>
+              </div>
 
-   <div className='champions'>
-        <div className='team1'>
-<div className='team1-container'>
-<img src={match.team1.teamIconUrl} width={35} alt={match.team1.teamName} />
-         <p key={match.team1.teamId}>{match.team1.shortName}</p>
-</div>
+              <div className="results">
+                <p>{risultato.pointsTeam1}</p> 
+                <p>vs</p>
+                <p>{risultato.pointsTeam2}</p> 
+              </div>
+
+              <div className="team2">
+                <img
+                  src={match.team2.teamIconUrl}
+                  width={35}
+                  alt={match.team2.teamName}
+                />
+              </div>
+              
+            </div>
+            <div className='marcatori'>
+               {match.goals.map((goal)=>(
+                <>
+                <li key={goal.goalID}>
+                Minuto: {goal.matchMinute}
+              </li>
+
+              <li key={goal.goalGetterID}>{goal.goalGetterName}</li>
+              <hr />
+                </>
+                
+              
+               ))}
+              </div>
+          </div>
          
-     </div>
- 
-     <div className='results'>
-        
-     <p key={match.matchResults.find((result)=>result.resultName==='Endergebnis')}>{match.matchResults.pointsTeam1}</p>
- <p>vs</p>
- <p key={match.matchResults.find((result)=>result.resultName==='Endergebnis')}>{match.matchResults.pointsTeam2}</p>
-     </div>
-     
- <div className='team2'>
- <img src={match.team2.teamIconUrl} width={35} alt={match.team2.teamName} />
- <p key={match.team2.teamId}>{match.team2.shortName}</p>
- 
- </div>
- 
- 
- 
-     </div>
-     
- 
+        );
+      })}
     </div>
- 
-))}
-
-
-        </div>
-    )
+  );
 }
