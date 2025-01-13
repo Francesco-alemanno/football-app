4aece7ab0733c6b1469f6b736efa0007297473c1
Francesco <alemannof43@gmail.com>
2025-01-13 15:31:56 +0100

    --

--- football-app/src/componenti/Champions.jsx
+++ football-app/src/componenti/Champions.jsx
@@ -15,18 +15,44 @@ if(isLoading){
     return(
         <div>
 <h3>Partite UCL 2024 </h3>
-
 {data.map((match)=>(
-    <div className='champions'>
-       <div className='team1'>
-        <img src={match.team1.teamIconUrl} width={40} alt={match.team1.teamName} />
-<p key={match.team1.teamId}>{match.team1.teamName}</p>
-    </div>
 
-<div className='team2'>
-<img src={match.team2.teamIconUrl} width={40} alt={match.team2.teamName} />
-<p key={match.team2.teamId}>{match.team2.teamName}</p>
+    <div>
+         
+        <div className='match-description'>
+            <p key={match.matchID}>{match.leagueName}</p>
+            <p key={match.matchID}>{match.group.groupName}</p>
+            <p key={match.matchID}>{new Date(match.matchDateTime).toLocaleString()}</p>
+            </div>
+
+
+   <div className='champions'>
+        <div className='team1'>
+<div className='team1-container'>
+<img src={match.team1.teamIconUrl} width={35} alt={match.team1.teamName} />
+         <p key={match.team1.teamId}>{match.team1.shortName}</p>
 </div>
+         
+     </div>
+ 
+     <div className='results'>
+        
+     <p key={match.matchResults.find((result)=>result.resultName==='Endergebnis')}>{match.matchResults.pointsTeam1}</p>
+ <p>vs</p>
+ <p key={match.matchResults.find((result)=>result.resultName==='Endergebnis')}>{match.matchResults.pointsTeam2}</p>
+     </div>
+     
+ <div className='team2'>
+ <img src={match.team2.teamIconUrl} width={35} alt={match.team2.teamName} />
+ <p key={match.team2.teamId}>{match.team2.shortName}</p>
+ 
+ </div>
+ 
+ 
+ 
+     </div>
+     
+ 
     </div>
  
 ))}
