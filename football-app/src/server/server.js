635ca29f95971ef06f4f16e7b2a026728cbfab04
unknown <alemannof43@gmail.com>
2025-03-26 12:00:29 +0100

    ulteriori modifie vercel

--- football-app/src/server/server.js
+++ football-app/src/server/server.js
@@ -1,41 +1,59 @@
-import express, { json } from 'express';
-import cors from 'cors';
-import { getAllUsers, login, registrazione, userLogged } from './controllers.js';
+import express, { json } from "express";
+import cors from "cors";
+import {
+  getAllUsers,
+  login,
+  registrazione,
+  userLogged,
+} from "./controllers.js";
 
-const app= express()
-const PORT= 5001;
+const app = express();
+const PORT = 5001;
 // middleware
-app.use(
-    cors({
-      origin: "*", // 🔴 Prova con "*" per accettare richieste da qualsiasi origine
-      methods: ["GET", "POST", "PUT", "DELETE"],
-      allowedHeaders: ["Content-Type", "Authorization"],
-      credentials: true, 
-    })
-  );
-  
+const allowedOrigins = [
+    'https://football-3kait4isd-francescos-projects-949cebc9.vercel.app',
+    'https://football-p3pjfzag0-francescos-projects-949cebc9.vercel.app',
+    'http://localhost:5174'
+  ];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+};
 
-app.use(json())
+// 2. Middleware CORS per tutte le routes
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
+
+app.use(json());
 
 // CHIAMATE
-app.get('/users', getAllUsers)
-app.post('/registrazione', registrazione)
+app.get("/users", getAllUsers);
+app.post("/registrazione", registrazione);
 
 // login
-app.post('/login', login)
-
+app.post("/login", login);
 
 // home
-app.get('/home/:userId', userLogged)
+app.get("/home/:userId", userLogged);
 
 app.use((req, res, next) => {
-    if (req.headers["x-forwarded-proto"] !== "https") {
-      return res.redirect(`https://${req.headers.host}${req.url}`);
-    }
-    next();
-  });
+  if (req.headers["x-forwarded-proto"] !== "https") {
+    return res.redirect(`https://${req.headers.host}${req.url}`);
+  }
+  next();
+});
 
 // listener
-app.listen(PORT, ()=>{
-    console.log(`Server in ascolto su http://localhost:${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Server in ascolto su http://localhost:${PORT}`);
+});
